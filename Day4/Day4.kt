c8ade238d9f6a9623665d2a21a4b15f7265946f6
traduongdatnguyen <tddnguyen.20it8@vku.udn.vn>
2022-08-07 19:11:31 +0700

    Create Day4.kt

---
+++ Day4/Day4.kt
@@ -0,0 +1,37 @@
+package com.example_chap3
+
+open class Person(age: Int, name: String) {
+    init {
+        println("My name is $name.")
+        println("My age is $age")
+    }
+}
+
+class MathTeacher(age: Int, name: String): Person(age, name) {
+
+    fun teachMaths() {
+        println("I teach in primary school.")
+    }
+}
+
+class Footballer(age: Int, name: String): Person(age, name) {
+    fun playFootball() {
+        println("I play for LA Galaxy.")
+    }
+}
+
+fun main(args: Array<String>) {
+    val t1 = MathTeacher(25, "Jack")
+    t1.teachMaths()
+
+    println()
+
+    val f1 = Footballer(29, "Christiano")
+    f1.playFootball()
+}
+
+
+
+
+
+
